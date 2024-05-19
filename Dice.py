f2971f9329bd0262c4c6c948eb5d01c8c868235a
TERUZ-VTAT <terunori.kato0911@gmail.com>
2024-05-19 13:02:19 +0900

    ダイスの保存機能の追加,フォント設定の一元化

--- Dice.py
+++ Dice.py
@@ -3,6 +3,33 @@ import time
 import socket
 import threading
 import random
+import os
+from pathlib import Path
+import csv
+import pandas as pd
+
+
+saved_dices = {}
+
+default_font = ("Arial", 14)
+
+# ↓アプリケーションのデータを保存するフォルダのセットアップ
+appdata_dir = fr"{Path(os.getenv('APPDATA')).parent}\\Local\\DiceApp\\"
+try:
+    os.makedirs(appdata_dir)
+except FileExistsError:
+    pass
+
+dataPaths = {
+    "saveddice":Path(fr"{appdata_dir}\\savedDice.csv")
+}
+
+# csvからセーブ済みのダイスをロード
+with open(dataPaths["saveddice"]) as f:
+    reader = csv.reader(f)
+    for c in reader:
+        if c != []:
+            saved_dices[int(c[0])] = (int(c[1]), int(c[2]))
 
 # ↓サーバーへの接続プログラム===================
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,9 +42,9 @@ player_color = None
 
 namewindow = tkinter.Tk()
 
-tkinter.Label(text="名前を入力してください", font=("Arial", 14)).pack()
+tkinter.Label(text="名前を入力してください", font=default_font).pack()
 
-nameentry = tkinter.Entry(font=("Arial", 14))
+nameentry = tkinter.Entry(font=default_font)
 nameentry.pack()
 
 def enter():
@@ -33,24 +60,31 @@ namewindow.mainloop()
 window = tkinter.Tk()
 window.title("ダイス")
 
-dicecanvas = tkinter.Canvas()
+main_canvas = tkinter.Canvas()
+main_canvas.pack(side="left",anchor="n")
+
+dicecanvas = tkinter.Canvas(main_canvas)
 dicecanvas.pack()
 
-tkinter.Label(dicecanvas, text="ダイス:", font=("Arial", 14)).pack(side="left", anchor="n")
+tkinter.Label(dicecanvas, text="ダイス:", font=default_font).pack(side="left", anchor="n")
 
-D_countBox = tkinter.Entry(dicecanvas, width=2, font=("Arial", 14))
+D_countBox = tkinter.Entry(dicecanvas, width=2, font=default_font)
 D_countBox.insert(0, "1")
 D_countBox.pack(side="left", anchor="n")
 
-tkinter.Label(dicecanvas, text="D", font=("Arial", 14)).pack(side="left", anchor="n")
+tkinter.Label(dicecanvas, text="D", font=default_font).pack(side="left", anchor="n")
 
-F_countBox = tkinter.Entry(dicecanvas, width=8, font=("Arial", 14))
+F_countBox = tkinter.Entry(dicecanvas, width=8, font=default_font)
 F_countBox.insert(0, "100")
 F_countBox.pack(side="left", anchor="n")
 
-def diceroll():
+def diceroll(d="", f=""):
     D_count = int(D_countBox.get())
     F_count = int(F_countBox.get())
+    if d != "":
+        D_count = int(d)
+    if f != "":
+        F_count = int(f)
     cache = []
     for _ in range(D_count):
         cache.append(random.randint(1, F_count))
@@ -73,14 +107,69 @@ dice_button.pack(side="left", anchor="n")
 private_dice_button = tkinter.Button(dicecanvas, text="プライベートダイスを振る", command=private_diceroll)
 private_dice_button.pack(side="left", anchor="n")
 
-tkinter.Label(text="ログ:", font=("Arial", 14)).pack(anchor="w")
+# ダイス保存とかの機能
+savedice_canvas = tkinter.Canvas()
+savedice_canvas.pack(side="left", anchor="n")
 
-logbox = tkinter.Text(font=("Arial", 14), state="disabled")
+tkinter.Label(savedice_canvas, text="保存したダイス", font=default_font).pack(anchor="e")
+
+def addsaveddice(id, D, F):
+    diceval_canvas = tkinter.Canvas(savedice_canvas)
+    diceval_canvas.pack(side="top",anchor="n")
+
+    diceval_entry = tkinter.Entry(diceval_canvas, font=default_font)
+    diceval_entry.insert(0, f"{D}D{F}")
+    diceval_entry.configure(state="readonly")
+    diceval_entry.pack(side="left")
+
+    roll_button = tkinter.Button(diceval_canvas, text="振る", command=lambda:diceroll(D, F))
+    roll_button.pack(side="left")
+
+    def delcanvas(id):
+        with open(dataPaths["saveddice"]) as f:
+            reader = csv.reader(f)
+            rowdata = [c for c in reader if c != [] and c[0] != str(id)]
+            print(rowdata)
+            print(type(rowdata))
+            with open(dataPaths["saveddice"], "w") as f2:
+                writer = csv.writer(f2)
+                for c in rowdata:
+                    writer.writerow(c)
+        diceval_canvas.destroy()
+
+    tkinter.Button(diceval_canvas, text="削除", command=lambda:delcanvas(id)).pack(side="left")
+
+def savedice():
+    D_count = int(D_countBox.get())
+    F_count = int(F_countBox.get())
+    print(saved_dices)
+    try:
+        id = max(list(saved_dices.keys()))+1
+    except ValueError:
+        id = 0
+    saved_dices[id] = (D_count, F_count)
+    addsaveddice(id, D_count, F_count)
+
+    if not dataPaths["saveddice"].exists():
+        dataPaths["saveddice"].touch()
+    with open(dataPaths["saveddice"], "a") as f:
+        writer = csv.writer(f)
+        writer.writerow([str(id), D_count, F_count])
+
+tkinter.Button(main_canvas, text="ダイスを保存", command=savedice).pack()
+
+for k, c in zip(list(saved_dices.keys()), list(saved_dices.values())):
+    addsaveddice(k, c[0], c[1])
+#=========================
+
+tkinter.Label(main_canvas, text="ログ:", font=default_font).pack(anchor="w")
+
+logbox = tkinter.Text(main_canvas, font=default_font, state="disabled")
 logbox.pack()
 
-chatframe = tkinter.Frame()
-tkinter.Label(chatframe, text="チャットを入力:", font=("Arial", 14)).pack(anchor="w", side="left")
-chatentry = tkinter.Entry(chatframe, font=("Arial", 14))
+chatframe = tkinter.Frame(main_canvas)
+tkinter.Label(chatframe, text="チャットを入力:", font=default_font).pack(anchor="w", side="left")
+chatentry = tkinter.Entry(chatframe, font=default_font)
 chatentry.pack(anchor="w", side="left")
 
 # [チャット欄が空欄],[送信した際の入力欄の内容削除],[enterを押した際にチャットを送信する機能]を追加
@@ -134,9 +223,9 @@ def type_event(event):
 # 名前を変更するときに出る新しいウィンドウとそのための関数
 def change_name():
     changewindow = tkinter.Toplevel()
-    tkinter.Label(changewindow, text="新しい名前を入力してください", font=("Arial", 14)).pack()
+    tkinter.Label(changewindow, text="新しい名前を入力してください", font=default_font).pack()
 
-    new_name_entry = tkinter.Entry(changewindow, font=("Arial", 14))
+    new_name_entry = tkinter.Entry(changewindow, font=default_font)
     new_name_entry.pack()
 
     def submit_new_name():
@@ -148,7 +237,7 @@ def change_name():
     tkinter.Button(changewindow, text="名前を変更", command=submit_new_name).pack()
 
 # 名前変更ボタンを追加
-change_name_button = tkinter.Button(window, text="名前を変更", command=change_name)
+change_name_button = tkinter.Button(main_canvas, text="名前を変更", command=change_name)
 change_name_button.pack()
 
 window.bind("<KeyPress>", type_event)
